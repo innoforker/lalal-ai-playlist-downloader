a1e9d920b59b8108f92a6c291c85137df07cbf88
Innoforker <innoforker@proton.me>
2025-02-13 16:32:11 +0000

    trash code was replaced

--- main.py
+++ main.py
@@ -7,24 +7,11 @@ from urllib.request import urlretrieve
 link = input("Enter the link to the playlist: ")
 file_name_segment = "segment-{}.mp3"
 
-def get_segment_padding_id(num_segment: int) -> str:
-    """
-    For generating segment numbers
-    """
-    segment_id = ""
-    if num_segment <= 9:
-        segment_id = f"00{num_segment}"
-    elif num_segment <= 99:
-        segment_id = f"0{num_segment}"
-    else:
-        segment_id = str(num_segment)
-    return segment_id or "000"
-
 for num_segment in range(1000):
     segment_link = link
     if not link.endswith("/"):
         segment_link += "/"
-    segment_name = file_name_segment.format(get_segment_padding_id(num_segment))
+    segment_name = file_name_segment.format(str(num_segment).zfill(3))
     segment_link += segment_name
     print(f"Downloading {segment_name}...")
     if get(segment_link).status_code == 200:
