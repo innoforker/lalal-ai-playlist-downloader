e0b97c8b3790fb30b216110b813402df9e6771f1
Innoforker <innoforker@proton.me>
2025-02-13 22:00:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from requests import get
 import os
 link = input("Enter the link to the playlist: ")
 file_name_segment = "segment-{}.mp3"
-all_files = sorted(os.listdir("."))
+all_files = sorted(os.listdir("segments"))
 last_segment = 0
 
 # [1] because [0] is "main.py"
-if all_files[1].startswith("segment"):
+if all_files[1].startswith("segments"):
         last_segment = int(os.path.splitext(all_files[1].replace("segment-", ""))) + 1
 
 for num_segment in range(last_segment, 1000):
@@ -22,7 +22,7 @@ for num_segment in range(last_segment, 1000):
     print(f"Downloading {segment_name}...")
     response = get(segment_link)
     if response.status_code == 200:
-        with open(segment_name, "wb") as file:
+        with open(f"segments/{segment_name}", "wb") as file:
             file.write(response.content)
     else:
         print(f"Can't download {segment_name}. Probably doesn't exist")
