d35516caa177cd96869475f9a63b5c30b17971d0
Innoforker <innoforker@proton.me>
2025-02-16 02:50:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ last_segment = 0
 
 for file in all_files:
         if file.startswith("segment-"):
-                last_segment = int(file[8:-4])
+                last_segment = int(file[8:-4]) # from segment- to .mp3
                 break
 
 for num_segment in range(last_segment, 1000):
